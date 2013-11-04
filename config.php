f49abdcde24d7b208c2cb04b43673088cd23b896
gifars <gifars@gmail.com>
2013-11-05 07:03:19 +0800

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 include_once("inc/facebook.php"); //include facebook SDK
  
 ######### edit details ##########
-$appId = '346921398777410'; //Facebook App ID
-$appSecret = '9ccc8b93e247f8457739ef85bd9991b9'; // Facebook App Secret
-$return_url = 'http://faisal.bl.ee';  //return url (url to script)
-$homeurl = 'http://faisal.bl.ee';  //return to home
+$appId = 'your_appID'; //Facebook App ID
+$appSecret = 'yoursecretkey'; // Facebook App Secret
+$return_url = 'your return url';  //return url (url to script)
+$homeurl = 'yout return url again';  //return to home
 $fbPermissions = 'publish_stream,manage_pages';  //Required facebook permissions
 ##################################
 
